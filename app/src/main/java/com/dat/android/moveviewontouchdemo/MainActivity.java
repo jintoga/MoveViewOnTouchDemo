3add5c959642e5ec5e70a3a641e5d4ec32618ff0
Nguyen Quoc Dat <jintoga123@yahoo.com>
2016-09-27 18:37:29 +0700

    1st

---
+++ app/src/main/java/com/dat/android/moveviewontouchdemo/MainActivity.java
@@ -0,0 +1,57 @@
+package com.dat.android.moveviewontouchdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements OnTouchListener {
+    RelativeLayout layout;
+    TextView textView;
+
+    private int _xDelta;
+    private int _yDelta;
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        final int X = (int) event.getRawX();
+        final int Y = (int) event.getRawY();
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                RelativeLayout.LayoutParams lParams =
+                    (RelativeLayout.LayoutParams) textView.getLayoutParams();
+                _xDelta = X - lParams.leftMargin;
+                _yDelta = Y - lParams.topMargin;
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                RelativeLayout.LayoutParams layoutParams =
+                    (RelativeLayout.LayoutParams) textView.getLayoutParams();
+                layoutParams.leftMargin = X - _xDelta;
+                layoutParams.topMargin = Y - _yDelta;
+                layoutParams.rightMargin = -250;
+                layoutParams.bottomMargin = -250;
+                textView.setLayoutParams(layoutParams);
+                break;
+        }
+        layout.invalidate();
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        layout = (RelativeLayout) findViewById(R.id.root);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setOnTouchListener(this);
+    }
+}
