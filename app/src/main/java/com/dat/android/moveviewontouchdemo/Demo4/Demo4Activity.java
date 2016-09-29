42414b1695c53c696533bd66e50e7210ad2acebf
Nguyen Quoc Dat <jintoga123@yahoo.com>
2016-09-29 17:23:09 +0700

    - added and using BoardView from DragListView library of Woxthebox - resolved problems:  + itemViews stack on top of each other  + only items on top of each column is draggable - code looks messy af

--- app/src/main/java/com/dat/android/moveviewontouchdemo/Demo4/Demo4Activity.java
+++ app/src/main/java/com/dat/android/moveviewontouchdemo/Demo4/Demo4Activity.java
@@ -1,32 +1,101 @@
 package com.dat.android.moveviewontouchdemo.Demo4;
 
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import com.dat.android.moveviewontouchdemo.Demo4.DragListView.BoardView;
+import com.dat.android.moveviewontouchdemo.Demo4.DragListView.DragItemRecyclerView;
 import com.dat.android.moveviewontouchdemo.R;
+import java.util.ArrayList;
 
 public class Demo4Activity extends AppCompatActivity {
 
-    @Bind(R.id.recyclerView1)
-    protected RecyclerView recyclerView1;
-    @Bind(R.id.recyclerView2)
-    protected RecyclerView recyclerView2;
-    private DraggableItemAdapter draggableItemAdapter;
+    private static int sCreatedItems = 0;
+    @Bind(R.id.boardView)
+    protected BoardView boardView;
+
+    private int mColumns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo4);
         ButterKnife.bind(this);
-        LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(this);
-        recyclerView1.setLayoutManager(linearLayoutManager1);
-        LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(this);
-        recyclerView2.setLayoutManager(linearLayoutManager2);
-        draggableItemAdapter = new DraggableItemAdapter();
-        recyclerView1.setAdapter(draggableItemAdapter);
-        recyclerView2.setAdapter(draggableItemAdapter);
+        boardView.setSnapToColumnsWhenScrolling(false);
+        boardView.setSnapToColumnWhenDragging(false);
+        boardView.setSnapDragItemToTouch(false);
+        boardView.setColumnWidth(300);
+        boardView.setBoardListener(new BoardView.BoardListener() {
+            @Override
+            public void onItemDragStarted(int column, int row) {
+                /*Toast.makeText(boardView.getContext(), "Start - column: " + column + " row: " + row,
+                    Toast.LENGTH_SHORT).show();*/
+            }
+
+            @Override
+            public void onItemChangedColumn(int oldColumn, int newColumn) {
+                TextView itemCount1 =
+                    (TextView) boardView.getHeaderView(oldColumn).findViewById(R.id.item_count);
+                itemCount1.setText(
+                    Integer.toString(boardView.getAdapter(oldColumn).getItemCount()));
+                TextView itemCount2 =
+                    (TextView) boardView.getHeaderView(newColumn).findViewById(R.id.item_count);
+                itemCount2.setText(
+                    Integer.toString(boardView.getAdapter(newColumn).getItemCount()));
+            }
+
+            @Override
+            public void onItemDragEnded(int fromColumn, int fromRow, int toColumn, int toRow) {
+                /*if (fromColumn != toColumn || fromRow != toRow) {
+                    Toast.makeText(boardView.getContext(),
+                        "End - column: " + toColumn + " row: " + toRow, Toast.LENGTH_SHORT).show();
+                }*/
+                MyDragItemAdapter adapterToColumn =
+                    (MyDragItemAdapter) boardView.getAdapter(toColumn);
+                MyDragItemAdapter adapterFromColumn =
+                    (MyDragItemAdapter) boardView.getAdapter(fromColumn);
+
+                DragItemRecyclerView fromRecyclerView =
+                    (DragItemRecyclerView) boardView.getRecyclerView(fromColumn);
+                fromRecyclerView.setDraggablePosition(adapterFromColumn.getDraggablePosition());
+
+                DragItemRecyclerView toRecyclerView =
+                    (DragItemRecyclerView) boardView.getRecyclerView(toColumn);
+                toRecyclerView.setDraggablePosition(adapterToColumn.getDraggablePosition());
+            }
+        });
+        addColumnList(5);
+        addColumnList(0);
+        addColumnList(0);
+        initDraggablePositions();
+    }
+
+    private void initDraggablePositions() {
+        for (int i = 0; i < mColumns; i++) {
+            ((DragItemRecyclerView) boardView.getRecyclerView(i)).setDraggablePosition(
+                ((MyDragItemAdapter) boardView.getAdapter(i)).getDraggablePosition());
+        }
+    }
+
+    private void addColumnList(int addItems) {
+        final ArrayList<Pair<Long, String>> mItemArray = new ArrayList<>();
+        for (int i = 0; i < addItems; i++) {
+            long id = sCreatedItems++;
+            mItemArray.add(new Pair<>(id, "Item " + id));
+        }
+        final MyDragItemAdapter listAdapter =
+            new MyDragItemAdapter(mItemArray, R.layout.column_item, R.id.item_layout, false);
+        final View header = View.inflate(this, R.layout.column_header, null);
+        ((TextView) header.findViewById(R.id.text)).setText("Column " + (mColumns + 1));
+        ((TextView) header.findViewById(R.id.item_count)).setText("" + addItems);
+
+        boardView.addColumnList(listAdapter, header, false);
+
+        mColumns++;
     }
 }
---
+++ app/src/main/java/com/dat/android/moveviewontouchdemo/Demo4/MyDragItemAdapter.java
@@ -0,0 +1,73 @@
+package com.dat.android.moveviewontouchdemo.Demo4;
+
+import android.support.v4.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.dat.android.moveviewontouchdemo.Demo4.DragListView.DragItemAdapter;
+import com.dat.android.moveviewontouchdemo.R;
+import java.util.ArrayList;
+
+/**
+ * Created by Nguyen on 9/28/2016.
+ */
+public class MyDragItemAdapter
+    extends DragItemAdapter<Pair<Long, String>, MyDragItemAdapter.ViewHolder> {
+    private int mLayoutId;
+    private int mGrabHandleId;
+    private boolean mDragOnLongPress;
+
+    public MyDragItemAdapter(ArrayList<Pair<Long, String>> list, int layoutId, int grabHandleId,
+        boolean dragOnLongPress) {
+        mLayoutId = layoutId;
+        mGrabHandleId = grabHandleId;
+        mDragOnLongPress = dragOnLongPress;
+        setHasStableIds(true);
+        setItemList(list);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(mLayoutId, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
+        String text = mItemList.get(position).second;
+        holder.mText.setText(text);
+        holder.itemView.setTag(text);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return mItemList.get(position).first;
+    }
+
+    public int getDraggablePosition() {
+        return mItemList.size() - 1;
+    }
+
+    public class ViewHolder extends DragItemAdapter.ViewHolder {
+        public TextView mText;
+
+        public ViewHolder(final View itemView) {
+            super(itemView, mGrabHandleId, mDragOnLongPress);
+            mText = (TextView) itemView.findViewById(R.id.text);
+        }
+
+        @Override
+        public void onItemClicked(View view) {
+            Toast.makeText(view.getContext(), "Item clicked", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public boolean onItemLongClicked(View view) {
+            Toast.makeText(view.getContext(), "Item long clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+    }
+}
